d8d6982a5c41ff705e7652a90cdec3d1477e54d3
AYUSH <singhayush9454@gmail.com>
2024-05-06 21:07:29 +0530

    6th may

--- occurance_counter.py
+++ occurance_counter.py
@@ -2,7 +2,8 @@ n=input("Numbers ")
 l=[]
 for i in n:
     l.append(i)
-l_uni=set(l)
+l_uni=sorted(set(l))
+print(l_uni)
 for i in l_uni:
     count=0
     for j in l:
---
+++ set.py
@@ -0,0 +1,4 @@
+s={2,4,2,6,9,8,8,3,6,7,4,1}
+print(set(s))
+# l=sorted(s)
+# print(l)
